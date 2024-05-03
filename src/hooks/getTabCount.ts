f4d90a6c5cd6cfc3371060c35e95aa9f9b21c95e
Abe Link <abelink@gmx.com>
2024-05-03 15:55:01 +0800

    getTabCount

--- src/hooks/getTabCount.ts
+++ src/hooks/getTabCount.ts
@@ -1,11 +1,42 @@
+import { useState, useEffect } from "react";
 import { useReadContract } from "wagmi";
-import { erc20Abi } from "viem";
-function getTokenName(address: any) {
-  const { data, error } = useReadContract({
-    address: address,
-    abi: erc20Abi,
-    functionName: "symbol",
+import { TAB_REGISTRY_CONFIG } from "../app/helpers/index";
+import { ethers } from "ethers";
+
+export const useTabCount = () => {
+  const [tabList, setTabList] = useState<string[]>([]);
+  const { data: activatedTabs, error } = useReadContract({
+    ...TAB_REGISTRY_CONFIG,
+    functionName: "activatedTabCount",
   });
-  return data;
-}
-export default getTokenName;
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const provider = new ethers.providers.JsonRpcProvider(
+          "https://sepolia-rollup.arbitrum.io/rpc"
+        );
+        const tabContract = new ethers.Contract(
+          TAB_REGISTRY_CONFIG.address,
+          TAB_REGISTRY_CONFIG.abi,
+          provider
+        );
+
+        const tabs: string[] = [];
+        if (activatedTabs) {
+          for (let i = 0; i < Number(activatedTabs); i++) {
+            let tab = await tabContract.tabList(i);
+            tabs.push(tab);
+          }
+        }
+        setTabList(tabs);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    fetchData();
+  }, [activatedTabs]);
+
+  return { tabList, error };
+};
